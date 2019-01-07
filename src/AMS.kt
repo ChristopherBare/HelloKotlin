a068a18ad4d680798aff0a4056bb66c91add05e5
christopher <christopherbare@outlook.com>
2019-01-07 03:36:29 -0500

    Did something a few days ago. I forgot.

--- src/AMS.kt
+++ src/AMS.kt
@@ -4,6 +4,8 @@ fun main(args: Array<String>) {
     println("Hello, world!")
     var day = dayOfTheWeek()
     println("Today is $day.")
+
+    feedTheFish()
 }
 
 fun dayOfTheWeek(): String {
@@ -18,4 +20,50 @@ fun dayOfTheWeek(): String {
         7 -> day =  "Saturday"
     }
     return day
+}
+
+fun feedTheFish() {
+    val day = dayOfTheWeek()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+}
+
+fun fishFood(day : String): String {
+
+    return when (day) {
+        "Monday" -> "Flakes"
+        "Tuesday" -> "Pellets"
+        "Wednesday" -> "Redworms"
+        "Thursday" -> "Granules"
+        "Friday" -> "Mosquitoes"
+        "Saturday" -> "Lettuce"
+        "Sunday" -> "Plankton"
+        else -> "nothing"
+    }
+
+}
+
+fun randomDay() : String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(7)]
+}
+
+fun swim(speed: String = "fast") {
+    println("Swimming $speed")
+}
+
+fun shouldChangeWater(
+        day: String,
+        temp: Int = 22,
+        dirty: Int = 20) {
+    when {
+        temp > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" -> true
+        else -> false
+    }
+}
+
+fun canAddFish(tankSize: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true): Boolean {
+    return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
